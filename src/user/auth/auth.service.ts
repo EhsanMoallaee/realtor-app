cb8f8752a6e10f53879ee153f37c3fd4f8e1c576
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-10 23:59:52 +0330

    Signup method done - need to improve for returning jwt token

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { Role } from '@prisma/client';
+
+
+interface ISignupParams {
+    name: string;
+    email: string;
+    phone: string;
+    password: string;
+}
 
 @Injectable()
 export class AuthService {
-    signup(body) {
-        console.log(body);
+
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async signup({name, email, phone, password}: ISignupParams) {
+        const userExist = await this.prismaService.user.findFirst({
+            where: { OR: [{email},{phone}] }
+        })
+        if(userExist) throw new ConflictException();
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await this.prismaService.user.create({
+            data: {
+                name,
+                email,
+                phone,
+                password: hashedPassword,
+                role: Role.BUYER
+            }
+        })
+        return user;
     }
 }
