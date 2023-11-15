12124c051f411ae750d9cee146cb367cafcd04d6
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-15 15:45:22 +0330

    Home controller test for getHomes filter construction implemented

--- src/home/home.controller.spec.ts
+++ src/home/home.controller.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeController } from './home.controller';
+import { HomeService } from './home.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('HomeController', () => {
-  let controller: HomeController;
+    let controller: HomeController;
+    let homeService: HomeService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [HomeController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [HomeController],
+            providers: [
+                {
+                    provide: HomeService,
+                    useValue: {
+                        getHomes: jest.fn().mockReturnValue([])
+                    }
+                },
+                PrismaService
+            ]
+        }).compile();
 
-    controller = module.get<HomeController>(HomeController);
-  });
+        controller = module.get<HomeController>(HomeController);
+        homeService = module.get<HomeService>(HomeService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    describe('getHomes', () => {
+        it('Should construct filters object correctly', async () => {
+            const mockGetHomes = jest.fn().mockReturnValue([]);
+            jest.spyOn(homeService, 'getHomes').mockImplementation(
+                mockGetHomes
+            );
+            await controller.getHomes('Mashhad', '1000000');
+
+            expect(mockGetHomes).toBeCalledWith({
+                city: 'Mashhad',
+                price: {
+                    gte: 1000000
+                }
+            });
+        });
+    });
 });
