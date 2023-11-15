8444a5b7c74284fccc67cf073ebdd2bf458e55a1
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-15 13:55:26 +0330

    GetHomes endpoint test implemented with mock prisma functions

--- src/home/home.service.spec.ts
+++ src/home/home.service.spec.ts
@@ -1,18 +1,76 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { HomeService } from './home.service';
+import { HomeService, homeSelect } from './home.service';
+import { PropertyType } from '@prisma/client';
+
+const mockGetHomes = [
+    {
+        id: 2,
+        address: 'Ahmad Abad',
+        city: 'Tehran',
+        price: 2000000,
+        propertyType: PropertyType.CONDO,
+        image: 'img4',
+        numberOfBedroomms: 2,
+        numberOfBathroomms: 1,
+        images: [{ url: 'img2' }]
+    }
+];
 
 describe('HomeService', () => {
-  let service: HomeService;
+    let service: HomeService;
+    let prismaService: PrismaService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [HomeService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                HomeService,
+                {
+                    provide: PrismaService,
+                    useValue: {
+                        home: {
+                            findMany: jest.fn().mockReturnValue(mockGetHomes)
+                        }
+                    }
+                }
+            ]
+        }).compile();
 
-    service = module.get<HomeService>(HomeService);
-  });
+        service = module.get<HomeService>(HomeService);
+        prismaService = module.get<PrismaService>(PrismaService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    describe('getHomes', () => {
+        const filters = {
+            city: 'Tehran',
+            price: {
+                gte: 1000000,
+                lte: 2000000
+            },
+            propertyType: PropertyType.CONDO
+        };
+
+        it('Call prisma home.findMany with correct parameters', async () => {
+            const mockPrismaFindManyHomes = jest
+                .fn()
+                .mockReturnValue(mockGetHomes);
+
+            jest.spyOn(prismaService.home, 'findMany').mockImplementation(
+                mockPrismaFindManyHomes
+            );
+            await service.getHomes(filters);
+            expect(mockPrismaFindManyHomes).toBeCalledWith({
+                select: {
+                    ...homeSelect,
+                    images: {
+                        select: {
+                            url: true
+                        },
+                        take: 1
+                    }
+                },
+                where: filters
+            });
+        });
+    });
 });
